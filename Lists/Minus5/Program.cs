5224c76a3f71ac2128b374c128514573459a2dcb
daddyAdrian <adrianlubenow@abv.bg>
2022-02-14 09:35:33 +0200

    added project RemoveFirst

---
+++ Lists/Minus5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minus5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
