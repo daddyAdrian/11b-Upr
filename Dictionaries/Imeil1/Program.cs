32ee9622e2aa5aeab9feebe758c0af4491a8952c
daddyAdrian <adrianlubenow@abv.bg>
2022-02-28 09:10:51 +0200

    added multiple projects

---
+++ Dictionaries/Imeil1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imeil1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> emails = new Dictionary<string, string>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                if(input[0] == "Stop")
+                {
+                    break;
+                }
+                else if (input[0] == "Add")
+                {
+                    if(emails.ContainsKey(input[1]))
+                    {
+                        emails[input[1]] = input[2];
+                    }
+                    else
+                    {
+                        emails.Add(input[1], input[2]);
+                    }
+                }
+                else if(input[0] == "Sent")
+                {
+                    if(emails.ContainsKey(input[1]))
+                    {
+                        Console.WriteLine($"{input[1]} {emails[input[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Rechnici/supermarket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
