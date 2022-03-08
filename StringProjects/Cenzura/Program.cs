72406f4773f2f43da139233776b0b96acbd1f876
daddyAdrian <adrianlubenow@abv.bg>
2022-03-08 12:22:17 +0200

    Added project "Cenzura"

---
+++ StringProjects/Cenzura/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Cenzura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(text);
+            sb.Replace(word, new string('*', word.Length));
+            Console.WriteLine(sb);
+        }
+    }
+}
