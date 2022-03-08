adcc52085d1a1d8eee508b576ef5e7aa98d9365c
daddyAdrian <adrianlubenow@abv.bg>
2022-03-08 12:15:14 +0200

    Finished project "StringProjects"

---
+++ StringProjects/Email/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = Console.ReadLine();
+            string[] details = email.Split('@').ToArray();
+            var sumLeftSymbols = SymSymbols(details[0]);
+            var sumRightSymbols = SymSymbols(details[1]);
+            if (sumRightSymbols - sumLeftSymbols >= 0)
+            {
+                Console.WriteLine("She is not the one.");
+            }
+            else
+            {
+                Console.WriteLine("Call her!");
+            }
+        }
+
+        private static int SymSymbols(string v)
+        {
+            var sum = 0;
+            for (int i = 0; i < v.Length; i++)
+            {
+                sum += (int)v[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ StringProjects/SBProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SBProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+            result.Append(str);
+            while(true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                if(input[0] =="Exit")
+                {
+                    break;
+                }
+                switch(input[0])
+                {
+                    case "Append": result.Append(input[1]);
+                        break;
+                    case "Remove": result.Remove(int.Parse(input[1]), int.Parse(input[2]));
+                        break;
+                    case "Insert": result.Insert(int.Parse(input[1]), input[2]);
+                        break;
+                    case "Replace": result.Replace(input[1], input[2]);
+                        break;
+                    default:break;
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
