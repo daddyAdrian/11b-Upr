f9b1daf406e09987ed8aa89b63e5b41973a0bf9d
daddyAdrian <adrianlubenow@abv.bg>
2022-05-10 12:01:17 +0300

    Added multiple projects

---
+++ ORMDemo1/ORMDemo1/DbFirstDemo/Program.cs
@@ -0,0 +1,127 @@
+﻿using DbFirstDemo.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace DbFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            DeleteJudgeSystem(context);
+            //AddNewProject(context);
+            //UpdateProject(context);
+        }
+        public static void DeleteJudgeSystem(SoftUniContext context)
+        {
+            var project =
+                context.Projects
+                .OrderByDescending(x => x.Name == "Judge System").First();
+            context.Projects.Remove(project);
+            context.SaveChanges();
+        }
+        public static void DeleteEmployeeProject(SoftUniContext context)
+        {
+            var employeeProject =
+                context.EmployeesProjects
+                .OrderByDescending(x => x.EmployeeId == 14).First();
+            context.EmployeesProjects.Remove(employeeProject);
+            context.SaveChanges();
+        }
+        public static void UpdateProject(SoftUniContext context)
+        {
+            var project = context.Projects.FirstOrDefault(x => x.Name == "Road-150");
+            project.Name = "Shkolo system";
+            project.StartDate = new DateTime(2021, 12, 22);
+            context.SaveChanges();
+        }
+        public static void AddNewProject(SoftUniContext context)
+        {
+            var project = new Project()
+            {
+                Name = "Judge System",
+                StartDate = DateTime.Now
+            };
+            context.Projects.Add(project);
+            context.SaveChanges();
+        }  
+        public static void AddNewTown(SoftUniContext context)
+        {
+            var town = new Town()
+            {
+                Name = "Plovdiv",
+            };
+            context.Towns.Add(town);
+            context.SaveChanges();
+        }
+        public static void AddEmployeeWithProject(SoftUniContext context)
+        {
+            var employee = new Employee()
+            {
+                FirstName = "Ani",
+                LastName = "Petrova",
+                JobTitle = "Designer",
+                HireDate = DateTime.UtcNow,
+                Salary = 2000,
+                DepartmentId = 2
+            };
+            context.Employees.Add(employee);
+            employee.EmployeesProjects.Add(new EmployeesProject
+            {
+                Project = new Project
+                {
+                    Name = "TTT",
+                    StartDate = DateTime.UtcNow
+                }
+            }
+             );
+            context.SaveChanges();
+        }
+        private static object GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                 .Where(x => x.Department.Name == "Research and Development")
+                 .Select(x => new
+                 {
+                     x.FirstName,
+                     x.LastName,
+                     x.Salary,
+                     DepartmentName = x.Department.Name,
+                 })
+                 .OrderBy(x => x.Salary)
+                 .ThenByDescending(x => x.FirstName)
+                 .ToList();
+            var sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName}from{employee.DepartmentName} - ${employee.Salary:F2}");
+            }
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+        private static object EmployeeCount(SoftUniContext context)
+        {
+            var employeesCount = context.Employees.Count();
+            return employeesCount;
+        }
+        private static object GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees.Where(x => x.Salary > 50000).Select(x => new
+            {
+                x.FirstName,
+                x.Salary
+            })
+                .OrderBy(x => x.FirstName).ToList();
+            var sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName}-{employee.Salary}");
+
+            }
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
---
+++ SqlInjection/SqlInjection/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data.SqlClient;
+
+
+namespace SqlInjection
+{
+    class Program
+    {
+        public static bool IsPasswordValid(string username, string password)
+        {
+            var dbConnection = "Data Source=.;Database=Test;Integrated Security=true;";
+            SqlConnection con = new SqlConnection(dbConnection);
+            con.Open();
+            string sql =
+                $"SELECT COUNT(*) FROM Users " +
+                $"WHERE Username = '{username}' AND " +
+                $"PasswordHash = '{password}'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            int matchedUsersCount = (int)cmd.ExecuteScalar();
+            con.Close();
+            return matchedUsersCount > 0;
+        }
+        static void Main(string[] args)
+        {
+            bool normalLogin = IsPasswordValid("peter", "qwerty123");
+
+            Console.WriteLine(normalLogin);
+
+            bool sqlInjectedLogin = IsPasswordValid(" ' or 1=1 --", "qwerty123");
+
+            Console.WriteLine(sqlInjectedLogin);
+
+            bool sqlInjectedLogin2 = IsPasswordValid(" ' or 1=1 --", " ' or 1=1 --");
+
+            Console.WriteLine(sqlInjectedLogin2);
+
+            bool evilHackerCreatesNewUser = IsPasswordValid("' INSERT INTO Users VALUES('hacker','') --","qwerty123");
+
+            Console.WriteLine(evilHackerCreatesNewUser);
+
+            bool evilHackerDropDatabase = IsPasswordValid("'; DROP TABLE Test; --", "qwerty123");
+
+            Console.WriteLine(evilHackerDropDatabase);
+        }
+
+    }
+}
