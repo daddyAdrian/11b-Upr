2a26a9c8c074b31a7fa088173a4aae12ac77fcf8
daddyAdrian <adrianlubenow@abv.bg>
2022-06-13 10:35:12 +0300

    Edited one project, and added one more

---
+++ LibraryApp/Data/LibraryContext.cs
@@ -0,0 +1,27 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public LibraryContext()
+        {
+            Database.EnsureCreated();
+        }
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Integrated Security=true;Database=LibraryDb");
+        }
+    }
+}
---
+++ LibraryApp/Data/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        [Range(1800, 2022)]
+        public int YearPublished { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Genre { get; set; }
+
+    }
+}
